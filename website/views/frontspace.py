e43d1d1624dec1d9dd89180581b3d5dac92a8ada
ymane <keita.souleyman225@gmail.com>
2022-06-09 15:24:16 +0000

    deplacer le

--- website/views/frontspace.py
+++ website/views/frontspace.py
@@ -16,7 +16,7 @@ def frontspace(request):
         if form.is_valid():
             form.save()
             #last_id=Voyage.objects.latest('id').id
-            return redirect('failled',form)
+            return redirect('success',form)
       
     context={
         'form':form,
