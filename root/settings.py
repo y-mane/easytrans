49e32115d96f685f0859c2ad5c52eece71746387
ymane <keita.souleyman225@gmail.com>
2022-06-16 13:24:28 +0000

    static_root

--- root/settings.py
+++ root/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
