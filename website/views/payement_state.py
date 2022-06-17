5505b35d79c14cb42efdc1cb829f7ed0096b83fb
ymane <keita.souleyman225@gmail.com>
2022-06-17 11:56:04 +0000

    custom data

--- website/views/payement_state.py
+++ website/views/payement_state.py
@@ -20,10 +20,7 @@ def failled(request):
     return render(request,'website/front/failled.html',context)
 
 def notification(request):
-    #if request.method=='POST':
-    #custom_data=request.POST.get('custom_data')
     custom_data=5
-    pprint(custom_data)
     voyage=Voyage.objects.get(id=custom_data)
     voyage.etat_paiement=Voyage.ETAT_PAIEMENT[0][0]
     context={
