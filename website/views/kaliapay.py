99c3822cce815009a425466c2d39d819bcd263d1
ymane <keita.souleyman225@gmail.com>
2022-06-25 21:18:49 +0000

    custom data avec request.body

--- website/views/kaliapay.py
+++ website/views/kaliapay.py
@@ -3,5 +3,5 @@ import requests
 
 endpoint = "http://146.190.238.183/payement_state/notification/"
 response = requests.post(endpoint, json={'custom_data':"1#success"})
-print(response.json())
+print(response)
 print(response.status_code)
