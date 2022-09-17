3948dbeff9c6a508639443e4711113e7bc87e6b5
ymane <keita.souleyman225@gmail.com>
2022-09-17 00:56:46 +0000

    avant soutenance

--- root/settings.py
+++ root/settings.py
@@ -72,9 +72,9 @@ WSGI_APPLICATION = 'root.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'easytrans_1',
+        'NAME': 'easytrans_3',
         'HOST':'localhost',
-        'USERNAME':'ymane',
+        'USERNAME':'keita',
         'PASSWORD':'thelifeislesgigas2020',
         'PORT': '5432',
 
